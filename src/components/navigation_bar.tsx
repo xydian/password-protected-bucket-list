637017df877ab0b76bf7be77aaecbe2381ce4356
Chris Böhm <chrisjb@webforeveryone.de>
2020-08-20 10:59:16 +0200

    added check mark and changed nav bar

--- src/components/navigation_bar.tsx
+++ src/components/navigation_bar.tsx
@@ -1,17 +1,56 @@
 import React from 'react'
-import { withStyles, TopNavigation, EvaProp } from '@ui-kitten/components'
+import { withStyles, TopNavigation, EvaProp, TopNavigationAction, Icon, OverflowMenu, MenuItem } from '@ui-kitten/components'
 import { StatusBar } from 'react-native'
 
 interface Props {
   eva?: EvaProp
 }
 
+const EditIcon = (props) => (
+  <Icon {...props} name='edit'/>
+);
+
+const MenuIcon = (props) => (
+  <Icon {...props} name='more-vertical'/>
+);
+
+const InfoIcon = (props) => (
+  <Icon {...props} name='info'/>
+);
+
+const LogoutIcon = (props) => (
+  <Icon {...props} name='log-out'/>
+);
+
 function NavigationBar(props: Props){
+  const [menuVisible, setMenuVisible] = React.useState(false);
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const renderMenuAction = () => (
+    <TopNavigationAction icon={MenuIcon} onPress={toggleMenu}/>
+  );
+
+  const renderRightActions = () => (
+    <React.Fragment>
+      <TopNavigationAction icon={EditIcon}/>
+      <OverflowMenu
+        anchor={renderMenuAction}
+        visible={menuVisible}
+        onBackdropPress={toggleMenu}>
+        <MenuItem accessoryLeft={InfoIcon} title='About'/>
+        <MenuItem accessoryLeft={LogoutIcon} title='Logout'/>
+      </OverflowMenu>
+    </React.Fragment>
+  );
+
   return (
     <TopNavigation
       // accessoryLeft={BackAction}
-      title='Eva Application'
-      style={{marginTop: StatusBar.currentHeight, borderBottomWidth: 1, borderBottomColor: props.eva.theme['xyz']}}
+      title='Bucket List'
+      style={{marginTop: StatusBar.currentHeight, borderBottomWidth: 1, borderBottomColor: props.eva.theme['color-basic-transparent-400']}}
+      accessoryRight={renderRightActions}
     />
   )
 }
--- src/screens/bucket_list.tsx
+++ src/screens/bucket_list.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { withStyles, Layout, ListItem, List, EvaProp, Divider } from '@ui-kitten/components'
+import { View } from 'react-native'
+import { withStyles, Layout, ListItem, List, EvaProp, Divider, CheckBox, StyleType, Toggle } from '@ui-kitten/components'
 
 interface BucketListItem {
+  id: number
   title: string 
   conditions: string 
   performed: boolean
@@ -9,21 +11,25 @@ interface BucketListItem {
 
 const listItems: BucketListItem[] = [
   {
+    id: 1, 
     title: 'Visit Spain', 
     conditions: 'Balearen zählen nicht', 
     performed: false 
   },
   {
+    id: 2, 
     title: 'Visit Spain123', 
     conditions: 'Balearen zählen nicht', 
     performed: true 
   },
   {
+    id: 3, 
     title: 'Visit Spain1234', 
     conditions: 'Balearen zählen nicht', 
     performed: false 
   },
   {
+    id: 4, 
     title: 'Visit Spain455', 
     conditions: 'Balearen zählen nicht', 
     performed: true 
@@ -39,12 +45,41 @@ function BucketListScreen(props: Props){
     <ListItem
       title={`${item.title} ${index + 1}`}
       description={`${item.conditions} ${index + 1}`}
-      appearance='alternative'
+      // appearance='alternative'
+      // accessory={(style: StyleType, index: number) => renderAccessory(style, item.id)}
+      accessoryRight={(props) => renderAccessory(props, item.id)}
     />
   );
 
+  const onCheckBoxCheckedChange = (id: number) => {
+
+  }
+
+  const renderAccessory = (props: any, id: number) => (
+    <CheckBox
+      {...props}
+      checked={true}
+      onChange={() => onCheckBoxCheckedChange(id)}
+    /> 
+  ); 
+
+  const [checked, setChecked] = React.useState(false);
+
+  const onCheckedChange = (isChecked) => {
+    setChecked(isChecked);
+  };
+
   return (
     <Layout level='2'>
+      <View style={{
+        alignItems: 'flex-start',
+        margin: 14, 
+      }}>
+        <Toggle checked={checked} onChange={onCheckedChange}>
+          Ausgeführte anzeigen
+        </Toggle>
+      </View>
+
       <List
         style={props.eva.style.container} 
         data={listItems}
